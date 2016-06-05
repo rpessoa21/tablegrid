01c94c207262c632ac582c775443f058b23864bd
Rodrigo Pessoa <rpessoa21@gmail.com>
2016-06-04 22:22:31 -0300

    First commit

---
+++ gulpfile.js
@@ -0,0 +1,60 @@
+// include the required packages. 
+var gulp = require('gulp');
+var jade = require('gulp-jade');
+var stylus = require('gulp-stylus');
+var uglify = require('gulp-uglify');
+var concat = require('gulp-concat');
+var watch = require('gulp-watch');
+
+// ================================
+// STYLUS
+// ================================
+gulp.task('stylus', function() {
+  return gulp.src('./assets/stylus/*.styl')
+  .pipe(stylus({
+    compress: true
+  }))
+  .pipe(gulp.dest('./app/css'));
+});
+
+
+// ================================
+// JADE
+// ================================
+gulp.task('jade', function() {
+  gulp.src('./assets/jade/**/*.jade')
+    .pipe(jade({
+        locals: {
+          title: 'OMG THIS IS THE TITLE'
+        },
+        pretty: true
+     }))
+     .pipe(gulp.dest('./app/'));
+});
+
+
+// ================================
+// UGLIFY
+// ================================
+gulp.task('js', function() {
+  return gulp.src('assets/js/*.js')
+    .pipe(uglify())
+    .pipe(concat('main.min.js'))
+    .pipe(gulp.dest('app/js'));
+});
+
+
+// ================================
+// WATCH
+// ================================
+gulp.task('watch', function() {
+	gulp.watch('./assets/stylus/*.styl', ['stylus']);
+	gulp.watch('./assets/jade/**/*.jade', ['jade']);
+  gulp.watch('assets/js/*.js', ['js']);
+});
+
+
+// ================================
+// DEFAULT
+// ================================
+gulp.task('default', ['watch']);
